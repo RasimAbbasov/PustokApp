dcfc4440080c7b04354f7757c6c24831eb9d377c
RasimAbbasov <rasimra@code.edu.az>
2025-07-21 18:39:29 +0400

    Features Identity,EmailConfirm

--- PustokApp/Models/BaseEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PustokApp.Models
-{
-    public class BaseEntity
-    {
-        public virtual int Id { get; set; }
-        public DateTime? CreateDate { get; set; }
-        public DateTime? UpdateDate { get; set; }
-    }
-}
